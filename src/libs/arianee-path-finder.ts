e4872a4d06de6619734ba910c538fe6fe8348fce
stefdelec <stephane@arianee.org>
2020-06-11 17:11:45 +0200

    feat(response): send is a middleware

--- src/libs/arianee-path-finder.ts
+++ src/libs/arianee-path-finder.ts
@@ -2,11 +2,12 @@ export const createRequestFromPathAndMethod = (method) => async (request, respon
     const parameters = Object.keys(request.body).length > 0 ? request.body : undefined;
     try {
         const result = await method(...parameters)
-        response.json(result);
+
         response.body = result;
         next();
     } catch (e) {
-        response.status(500).json(e.toString());
+        console.error(e);
+        response.body=e.toString();
         request.inError = true;
         console.error(e);
         next();
